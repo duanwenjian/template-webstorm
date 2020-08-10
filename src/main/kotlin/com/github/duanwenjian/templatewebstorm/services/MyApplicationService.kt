b56cf8fcbdf3689b55d408c0a00717ea7081bc13
GitHub Action <action@github.com>
2020-08-10 06:54:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/duanwenjian/templatewebstorm/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.duanwenjian.templatewebstorm.services
+
+import com.github.duanwenjian.templatewebstorm.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
